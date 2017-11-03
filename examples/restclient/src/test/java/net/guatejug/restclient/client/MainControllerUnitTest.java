6c0e9f83d42e295b950324e9785e20147a28a402
Erik Giron <erik.giron@xoom.com>
2017-11-03 15:18:35 -0700

    add mock

--- examples/restclient/src/test/java/net/guatejug/restclient/client/MainControllerUnitTest.java
+++ examples/restclient/src/test/java/net/guatejug/restclient/client/MainControllerUnitTest.java
@@ -2,7 +2,9 @@ package net.guatejug.restclient.client;
 
 
 import net.guatejug.restclient.main.MainController;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -10,6 +12,14 @@ import static org.hamcrest.core.Is.is;
 public class MainControllerUnitTest {
 
     private MainController mainController;
+
+    private RestClient restClient;
+
+    @Before
+    public void setup(){
+        Mockito.mock(restClient.getClass());
+        mainController = new MainController(restClient);
+    }
 
     @Test
     public void testEscribirCosa() {
